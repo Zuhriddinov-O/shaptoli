d50c9275caf1d0e4230b265ae2f2cd33ae2f4393
Z-oybek <newprogrammer2023@gmail.com>
2024-03-28 22:45:43 +0500

    22:45

--- lib/pages/categories/main_page.dart
+++ lib/pages/categories/main_page.dart
@@ -1,32 +1,35 @@
 import 'package:contained_tab_bar_view/contained_tab_bar_view.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
-import 'package:gap/gap.dart';
+import 'package:flutter/widgets.dart';
 import 'package:uzum_market_project/classes/products.dart';
+
 import '../../widgets/scrollable_categories.dart';
 import '../../widgets/slider.dart';
-import '../appBar.dart';
 
 class MainPage extends StatefulWidget {
-  MainPage({super.key});
+  const MainPage({super.key});
 
   @override
   State<MainPage> createState() => _MainPageState();
 }
 
 class _MainPageState extends State<MainPage> {
+  bool saveProduct = false;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: appBar(context, true),
+      // appBar: appBar(context, true),
       body: RefreshIndicator(
         onRefresh: () async {
           setState(() {});
         },
         child: ListView(
           children: [
-            container(context),
-            singleChildScrollView(context),
+            // container(context),
+            // singleChildScrollView(context),
             Container(
               height: 750,
               width: double.infinity,
@@ -53,70 +56,96 @@ class _MainPageState extends State<MainPage> {
     );
   }
 
-  Container _container() {
-    return Container(
-      color: Colors.transparent,
-      child: GridView.builder(
-        gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-          crossAxisCount: kIsWeb ? 5 : 2,
-          mainAxisExtent: 500,
-          crossAxisSpacing: 12,
-          mainAxisSpacing: 12,
-        ),
-        itemCount: productList.length,
-        itemBuilder: (BuildContext context, index) {
-          productList.shuffle();
-          final items = productList[index];
-          return InkWell(
-            onTap: () {},
-            child: Ink(
-              color: Colors.blue,
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Center(
-                      child: ClipRRect(
-                        borderRadius: BorderRadius.circular(12),
-                        child: Image.network(items.image,
-                            fit: BoxFit.fill, width: MediaQuery.of(context).size.width),
-                      )),
-                  Padding(
-                    padding: const EdgeInsets.all(8.0),
-                    child: Text(items.name, overflow: TextOverflow.ellipsis),
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.all(8.0),
-                    child: Row(
-                      children: [
-                        Image.asset("assets/logos/star.png", width: 13, height: 13),
-                        Text(items.rating.toString()),
-                      ],
-                    ),
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.only(left: 10, top: 8, bottom: 8),
-                    child: Column(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        Text(
-                          "${items.price} so'm",style: const TextStyle(decoration: TextDecoration.lineThrough),),
-                        Text(
-                            "${items.discount} so'm",
-                        ),
-                      ],
-                    ),
-                  ),
-                  Center(child: Image.network(items.image,fit: BoxFit.fill,width: MediaQuery.of(context).size.width)),
-                  Text(items.name,overflow:TextOverflow.ellipsis),
-                  Text(items.rating.toString()),
-                  Text(items.price.toString(), style: const TextStyle(decoration: TextDecoration.lineThrough, fontSize: 12),),
-                  Text(items.discount.toString()),
-                ],
-              ),
-            ),
-          );
-        },
+  Widget _container() {
+    return GridView.builder(
+      padding: const EdgeInsets.all(20),
+      gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+        crossAxisCount: kIsWeb ? 5 : 2,
+        mainAxisExtent: MediaQuery.of(context).size.height/1.4,
+        crossAxisSpacing: 12,
+        mainAxisSpacing: 12,
       ),
+      itemCount: productList.length,
+      itemBuilder: (BuildContext context, index) {
+        productList.shuffle();
+        final items = productList[index];
+        return InkWell(
+          onTap: () {},
+          child: Ink(
+            color: Colors.blue,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Center(
+                  child: ClipRRect(
+                    borderRadius: BorderRadius.circular(12),
+                    child: Container(
+                      width: MediaQuery.of(context).size.width,
+                      color: Colors.red,
+                      child: Image.network(
+                        items.image,
+                        height: MediaQuery.of(context).size.height/2,
+                        fit: BoxFit.fill,
+                      ),
+                    ),
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Text(items.name, overflow: TextOverflow.ellipsis),
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(left: 8.0),
+                  child: Row(
+                    children: [
+                      Image.asset("assets/logos/star.png", width: 13, height: 13),
+                      Text(items.rating.toString()),
+                    ],
+                  ),
+                ),
+                Padding(
+                  padding: EdgeInsets.only(left: 10),
+                  child: Column(
+                    children: [
+                      Align(
+                        alignment: Alignment.centerLeft,
+                        child: Text(
+                          "${items.price} so'm",
+                          style: const TextStyle(decoration: TextDecoration.lineThrough),
+                        ),
+                      ),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Text(
+                            "${items.discount} so'm",
+                          ),
+                          IconButton(
+                            onPressed: () {
+                              ScaffoldMessenger.of(context).hideCurrentSnackBar();
+                              saveProduct = !saveProduct;
+                              ScaffoldMessenger.of(context).showSnackBar(
+                                SnackBar(
+                                  backgroundColor: saveProduct ? null : Colors.red,
+                                  content: Text(saveProduct
+                                      ? "Product is added Succesfully"
+                                      : "Product is removed from the list"),
+                                ),
+                              );
+                            },
+                            icon: Image.asset("assets/logos/shopping-bag2.png",
+                                width: 30, height: 30, color: Colors.grey),
+                          )
+                        ],
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          ),
+        );
+      },
     );
   }
 }
